a73109e48e5243edda84b1c9761612051bd88304
csupreme19 <csupreme19@gmail.com>
2022-11-18 19:29:14 +0900

    JDBC 커넥션 사용해보기

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,9 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+    static final String URL = "jdbc:h2:tcp://localhost/~/git/study/h2/test";
+    static final String USERNAME = "sa";
+    static final String PASSWORD = "";
+
+}
