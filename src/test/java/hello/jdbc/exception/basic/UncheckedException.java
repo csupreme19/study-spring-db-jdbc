4bd26f99ad6986a5767bcb09a28d92fb24b800cf
csupreme19 <csupreme19@gmail.com>
2022-11-22 02:54:48 +0900

    자바 예외 - 언체크 예외

---
+++ src/test/java/hello/jdbc/exception/basic/UncheckedException.java
@@ -0,0 +1,54 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class UncheckedException {
+
+    @Test
+    void unchecked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        Assertions.assertThatThrownBy(service::callThrow)
+                .as("MyUncheckedException 예외 발생")
+                .isInstanceOf(MyUncheckedException.class);
+    }
+
+    static class MyUncheckedException extends RuntimeException {
+        public MyUncheckedException(String message) {
+            super(message);
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyUncheckedException e) {
+                log.info("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        public void callThrow() {
+            repository.call();
+        }
+    }
+
+    /**
+     * Unchecked Exception은 예외를 던지지 않아도 자동으로 던진다.
+     */
+    static class Repository {
+        public void call() {
+            throw new MyUncheckedException("ex");
+        }
+    }
+}
